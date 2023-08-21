ad6008bbf01ac1a28957a6611ba4605cefac63b0
Sébastien VIAL <sebastien@vial.design>
2023-08-21 16:50:37 +0200

    feat: custom user

--- hosts/default.nix
+++ hosts/default.nix
@@ -10,14 +10,14 @@
           inherit system;
 
           modules = [
-            ./linudev
+            ./mizu
             ../modules/desktop.nix
             #../modules/greetd.nix
             ../modules/desktop.nix
             # ../modules/lanzaboote.nix
             {
-              home-manager.users.linuxmobile.imports =
-                homeImports."linuxmobile@linudev";
+              home-manager.users.sebastien.imports =
+                homeImports."sebastien@mizu";
             }
             systemInputs
           ] ++ sharedModules;
